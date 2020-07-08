fc11e5902a19d66ac04898dbc0bdd09d7f0558b3
stropepk <stropepk@nih.gov>
2020-07-08 19:18:32 +0000

    adding protein commands

---
+++ get_proteins.sh
@@ -0,0 +1,11 @@
+../datasets download gene 59272, 70008, 492331, 712790, 112313373 --filename ACE2.zip
+unzip ACE2.zip -d ACE2
+cat ACE2/ncbi_dataset/data/protein.faa
+
+../datasets download gene 28, 80908, 722252, 112320051 --filename ABO.zip
+unzip ABO.zip -d ABO
+cat ABO/ncbi_dataset/data/protein.faa
+
+../datasets download gene 7113, 50528, 494080, 715138, 112306012 --filename TMPRSS2.zip
+unzip TMPRSS2.zip -d TMPRSS2
+cat TMPRSS2/ncbi_dataset/data/protein.faa
