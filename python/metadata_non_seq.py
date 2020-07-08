1c028e2f866fd139916ad386b0497f68086a4cb7
Zhang, Xuan <zhangxua@ncbi.nlm.nih.gov>
2020-07-08 17:30:43 -0400

    get key fields from esummary

--- python/metadata_non_seq.py
+++ python/metadata_non_seq.py
@@ -7,37 +7,47 @@ https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=28
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 from Bio import Entrez
+import urllib.request
 
-HTTP_PREFIX = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id="
+HTTP_PREFIX = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&retmode=xml&id="
 
 
-class GeneSummary():
+class GeneFetch():
     def __init__(self, gene_id):
         Entrez.email = "codeathon@example.com"
-        self.handle = Entrez.esummary(db="gene", id=gene_id)
-        # Entrez.esummary(db="gene", id="28")
+        self.handle_summary = Entrez.esummary(db="gene", id=gene_id, rettype="xml")
+        self.handle_full = Entrez.efetch(db="gene", id=gene_id, rettype="xml")
+        # webUrl = urllib.request.urlopen('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=28')
+        # webUrl.getcode() == 200
+        # data = webUrl.read()
 
     def record(self):
-        record = Entrez.read(self.handle, validate=False)
-        return record
+        record_summary = Entrez.read(self.handle_summary)
+        record_full = Entrez.read(self.handle_full)
+        return record_summary['DocumentSummarySet']['DocumentSummary'][0], record_full[0]
+        # dom = minidom.parseString(data)
 
 
 class GeneData():
     def __init__(self, gene_id):
-        gene_sum = GeneSummary(gene_id)
-        record = gene_sum.record()
-        self.doc = record['DocumentSummarySet']['DocumentSummary'][0]
+        gene_record = GeneFetch(gene_id)
+        self.doc_summary, self.doc_full = gene_record.record()
+        # self.doc = record['DocumentSummarySet']['DocumentSummary'][0]
 
     def print_field(self, name, mapped_key):
-        print(name + '\t' + self.doc[mapped_key])
-    
+        in_summary, field_name = mapped_key
+        if in_summary:
+            print(name + '\t' + self.doc_summary[field_name])
+        # e = dom.getElementsByTagName('Entrezgene_summary')
+        # print(e[0].childNodes[0].data)
+
 
 if __name__ == "__main__":
     data = GeneData(28)
-    value_list = {'Summary': 'Summary',
-                  'Symbol': 'Name',
-                  'Aliases': 'OtherAliases',
-                  'Description': 'Description',
+    value_list = {'Summary': (True, 'Summary'),
+                  'Symbol': (True, 'Name'),
+                  'Aliases': (True, 'OtherAliases'),
+                  'Description': (True, 'Description'),
                   }
     for key, val in value_list.items():
         data.print_field(key, val)
