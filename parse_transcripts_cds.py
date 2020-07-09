9d7fe1d5d48854c7b80938da4351cee2aff93969
Andrew <akinhwan@gmail.com>
2020-07-09 14:31:11 +0000

    Write name, score, strand. Add requirements.txt

--- parse_transcripts_cds.py
+++ parse_transcripts_cds.py
@@ -15,11 +15,20 @@ accessionVersion = parsed_yaml_file["genes"][0]['genomicRanges'][0]['accessionVe
 chromStart = parsed_yaml_file["genes"][0]["genomicRanges"][0]["range"][0]["begin"]
 chromEnd = parsed_yaml_file["genes"][0]["genomicRanges"][0]["range"][0]["end"]
 orientation = parsed_yaml_file["genes"][0]["genomicRanges"][0]["range"][0]["orientation"]
+geneId = parsed_yaml_file["genes"][0]["geneId"]
+symbol = parsed_yaml_file["genes"][0]["symbol"]
+name = f"GeneID:{geneId}_{symbol}"
+score = 1
+
+if orientation == "positive":
+    strand = '+'
+elif orientation == "minus":
+    strand = '-'
 
 pp.pprint(cds)
 # prints out the following...
 # {'accessionVersion': 'NM_021804.3', 'range': [{'begin': '307', 'end': '2724'}]}
 
-bed_file.write(f"{accessionVersion}    {chromStart}    {chromEnd}\n")
+bed_file.write(f"{accessionVersion}    {chromStart}    {chromEnd}    {name}    {score}    {strand}\n")
 a_yaml_file.close()
 bed_file.close()
