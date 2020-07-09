96a9d1e0175949a8a4a64b2f4bafca5e80adbaa2
Wratko Hlavina <whlavina@ncbi.nlm.nih.gov>
2020-07-09 20:22:00 +0000

    Parameterize get_proteins script and invoke it from main Python.

--- get_proteins.sh
+++ get_proteins.sh
@@ -1,61 +1,34 @@
+#!/bin/bash
 
-#get protein seq
+datasets_dir=$1
+output_dir=$2
 
-../datasets download gene 59272 70008 492331 712790 112313373 --filename ACE2.zip
-unzip ACE2.zip -d ACE2
-#ls ACE2/ncbi_dataset/data/protein.faa
+usage() {
+        echo "Usage: $0 <DATASETS-DIR> <OUTPUT-DIR>"
+        exit 1
+}
 
-../datasets download gene 28, 80908, 722252, 112320051 --filename ABO.zip
-unzip ABO.zip -d ABO
-#ls ABO/ncbi_dataset/data/protein.faa
+[[ $# == 2 ]] || usage
 
-../datasets download gene 7113, 50528, 494080, 715138, 112306012 --filename TMPRSS2.zip
-unzip TMPRSS2.zip -d TMPRSS2
-#ls TMPRSS2/ncbi_dataset/data/protein.faa
+protein_fasta=$datasets_dir/ncbi_dataset/data/protein.faa
+[[ -r $protein_fasta ]] || {
+       echo >&2 "ERROR: Must provide input to NCBI Datasets gene data with protein.faa"
+       exit 1
+}
 
-
-#extract regions of protein seq
-
-echo -n "" > tempRegion
-
-for i in $(grep '>'  ./ABO/ncbi_dataset/data/protein.faa | cut -f 1 -d ' '| sed 's/>//')
-do
-        echo $i
-        #esearch -db protein -query $i |efetch -format gpc|xtract -insd Region   region_name INSDInterval_from INSDInterval_to sub_sequence | awk '{print "Region\t"$0}'
-        esearch -db protein -query $i |efetch -format gpc|xtract -insd Region   region_name INSDInterval_from INSDInterval_to sub_sequence | awk '{print "Region\t"$0}' >> tempRegion
-        esearch -db protein -query $i |efetch -format gpc|xtract -insd sig_peptide   region_name INSDInterval_from INSDInterval_to sub_sequence | awk '{print "sig_peptide\t"$0}' >> tempRegion
-        esearch -db protein -query $i |efetch -format gpc|xtract -insd mat_peptide   product INSDInterval_from INSDInterval_to sub_sequence | awk '{print "mat_peptide\t"$0}' >> tempRegion
-
-done
-
-paste tempRegion <(cat tempRegion| cut -f 3 | cut -f 1 -d '/') | awk  'BEGIN {FS="\t"}{print ">"$2"|"$1"|"$4"-"$5"|"$8"\n"$6}' > ABO_feature.fasta
-
+[[ -d $output_dir ]] || {
+       echo >&2 "ERROR: Must provide output directory"
+       exit 1
+}
 
 echo -n "" > tempRegion
 
-for i in $(grep '>'  ./ACE2/ncbi_dataset/data/protein.faa | cut -f 1 -d ' '| sed 's/>//')
+for i in $(grep '>' "$datasets_dir/ncbi_dataset/data/protein.faa" | cut -f 1 -d ' '| sed 's/>//')
 do
         echo $i
         esearch -db protein -query $i |efetch -format gpc|xtract -insd Region   region_name INSDInterval_from INSDInterval_to sub_sequence | awk '{print "Region\t"$0}' >> tempRegion
         esearch -db protein -query $i |efetch -format gpc|xtract -insd sig_peptide   region_name INSDInterval_from INSDInterval_to sub_sequence | awk '{print "sig_peptide\t"$0}' >> tempRegion
         esearch -db protein -query $i |efetch -format gpc|xtract -insd mat_peptide   product INSDInterval_from INSDInterval_to sub_sequence | awk '{print "mat_peptide\t"$0}' >> tempRegion
-
 done
 
-paste tempRegion <(cat tempRegion| cut -f 3 | cut -f 1 -d '/') | awk  'BEGIN {FS=OFS="\t"}{print ">"$2"|"$1"|"$4"-"$5"|"$8"\n"$6}' > ACE2_feature.fasta
-
-
-echo -n "" > tempRegion
-
-for i in $(grep '>'  ./TMPRSS2/ncbi_dataset/data/protein.faa | cut -f 1 -d ' '| sed 's/>//')
-do
-        echo $i
-        esearch -db protein -query $i |efetch -format gpc|xtract -insd Region   region_name INSDInterval_from INSDInterval_to sub_sequence | awk '{print "Region\t"$0}' >> tempRegion
-        esearch -db protein -query $i |efetch -format gpc|xtract -insd sig_peptide   region_name INSDInterval_from INSDInterval_to sub_sequence | awk '{print "sig_peptide\t"$0}' >> tempRegion
-        esearch -db protein -query $i |efetch -format gpc|xtract -insd mat_peptide   product INSDInterval_from INSDInterval_to sub_sequence | awk '{print "mat_peptide\t"$0}' >> tempRegion
-
-done
-
-paste tempRegion <(cat tempRegion| cut -f 3 | cut -f 1 -d '/') | awk  'BEGIN {FS=OFS="\t"}{print ">"$2"|"$1"|"$4"-"$5"|"$8"\n"$6}' > TMPRSS2_feature.fasta
-
-#ls ABO_feature.fasta ACE2_feature.fasta TMPRSS2_feature.fasta
+paste tempRegion <(cat tempRegion| cut -f 3 | cut -f 1 -d '/') | awk  'BEGIN {FS="\t"}{print ">"$2"|"$1"|"$4"-"$5"|"$8"\n"$6}' > "$output_dir/feature.fasta"
